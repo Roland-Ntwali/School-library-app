7350497cce21a241903b56f139aad3d400386d98
Roland-Ntwali <rolandntwali@gmail.com>
2023-06-20 16:30:21 +0200

    Implement correct name method

---
+++ base_decorator.rb
@@ -0,0 +1,14 @@
+require 'nameable'
+
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initilize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
