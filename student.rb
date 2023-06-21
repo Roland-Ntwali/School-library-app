465c2fe6709a98d7104c8f358745378eeea07c93
Roland-Ntwali <rolandntwali@gmail.com>
2023-06-21 13:46:32 +0200

    relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     ' ¯\(ツ)/¯ '
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
