12341d8601bc320455337189b4f9829d2d8f3338
Roland-Ntwali <rolandntwali@gmail.com>
2023-06-26 14:49:24 +0200

    preserving data for the books

--- data/data.rb
+++ data/data.rb
@@ -4,3 +4,45 @@ require_relative '../student'
 require_relative '../teacher'
 require_relative '../app'
 require 'json'
+
+def load_books
+  return unless File.exist?('./data/books.json')
+
+  file = File.open('./data/books.json')
+
+  if file.empty?
+    'Please add some books.'
+  else
+    books = JSON.parse(File.read('./data/books.json'))
+
+    books.each do |book|
+      book = Book.new(book['title'], book['author'])
+      @books << book
+    end
+  end
+  file.close
+end
+
+def save_book(title, author)
+  obj = {
+    title: title,
+    author: author
+  }
+
+  return unless File.exist?('./data/books.json')
+
+  file = File.open('./data/books.json')
+
+  if file.empty?
+    book = [obj]
+  else
+    book = JSON.parse(File.read('./data/books.json'))
+    book << obj
+  end
+
+  file.close
+
+  myfile = File.open('./data/books.json', 'w')
+  myfile.write(JSON.pretty_generate(book))
+  myfile.close
+end
