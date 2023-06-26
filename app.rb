f72242ddcf5f71adc67fbea1416b7e355608b615
Ouail-01 <ouaillaarabi@gmail.com>
2023-06-26 11:05:02 +0100

    Add comments to the app,student and teacher files

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require_relative './rental'
 
 class App
   def initialize
+    # print commands
     @cmd = {
       '1': 'List all books',
       '2': 'List all people',
@@ -15,17 +16,21 @@ class App
       '7': 'Exit'
     }
 
+    # list of books
     @books = []
 
+    # list of people
     @people = []
   end
 
+  # display commands
   def display_cmd
     @cmd.each do |index, command|
       puts "#{index} - #{command}"
     end
   end
 
+  # print out all books
   def list_all_books
     if @books.empty?
       puts 'Please insert books first!'
@@ -36,6 +41,7 @@ class App
     end
   end
 
+  # print out all people
   def list_all_people
     if @people.empty?
       puts 'Please insert people first!'
@@ -46,12 +52,14 @@ class App
     end
   end
 
+  # print out all people with numbers
   def list_all_person_with_numbers
     @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
+  # create a student
   def create_student
     print 'Age: '
     age = gets.chomp
@@ -66,9 +74,11 @@ class App
     when 'n' then parent_permission = false
     end
 
+    # create a student and push it to people array
     @people.push(Student.new('11', age, name, parent_permission: parent_permission))
   end
 
+  # create a teacher
   def create_teacher
     print 'Age: '
     age = gets.chomp
@@ -79,9 +89,11 @@ class App
     print 'Specialization: '
     specialization = gets.chomp
 
+    # create a teacher and push it to people array
     @people.push(Teacher.new(specialization, age, name))
   end
 
+  # create a person
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     case gets.chomp
@@ -92,9 +104,11 @@ class App
       create_teacher
     end
 
+    # success message
     puts 'Person created successfully'
   end
 
+  # create a book
   def create_book
     print 'Title: '
     title = gets.chomp
@@ -102,35 +116,46 @@ class App
     print 'Author: '
     author = gets.chomp
 
+    # create the book object and add it to the books list
     @books.push(Book.new(title, author))
 
+    # success message
     puts 'Book created successfully'
   end
 
+  # create a rental
   def create_rental()
+    # select book
     puts 'Select a book from the following list by number: '
     list_all_books
     book_option = gets.chomp.to_i
     puts
 
+    # select person
     puts 'Select a person from the following list by number (not id): '
     list_all_person_with_numbers
     person_option = gets.chomp.to_i
     puts
 
+    # select date
     print 'Date: '
     date = gets.chomp
 
+    # create the book object and add it to the books list
     Rental.new(date, @books[book_option], @people[person_option])
 
+    # success message
     puts 'Rental created successfully'
   end
 
+  # list all rentals for a given person id
   def rentals_of_person()
+    # select person
     print 'Id of person: '
     id = gets.chomp.to_i
     person_arr = @people.select { |person| person.id == id }
 
+    # print rentals for that person
     if person_arr.empty?
       puts 'No person matches the given ID!'
     else
@@ -140,6 +165,7 @@ class App
     end
   end
 
+  # options
   def choose_option(input)
     case input
     when '1' then list_all_books
@@ -151,6 +177,7 @@ class App
     end
   end
 
+  # user option selector
   def user_option_executer
     input = gets.chomp
 
@@ -161,6 +188,7 @@ class App
     end
   end
 
+  # run the program
   def run
     puts 'Please choose an option by entering a number: '
     display_cmd
--- student.rb
+++ student.rb
@@ -1,17 +1,21 @@
 require_relative 'person'
 
 class Student < Person
+  # getters & setters
   attr_accessor :classroom
 
+  # constructor
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
+  # method
   def play_hooky
     '¯\(ツ)/¯'
   end
 
+  # add classroom method
   def add_classroom(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
